5b02714b077a39994c93a53042cf071c789172b1
pyt <2630451673@qq.com>
2019-01-19 16:26:03 +0800

    初始化添加数据库信息打印

--- src/main/java/com/hydata/intelligence/platform/service/InitializingService.java
+++ src/main/java/com/hydata/intelligence/platform/service/InitializingService.java
@@ -1,6 +1,9 @@
 package com.hydata.intelligence.platform.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +17,21 @@ public class InitializingService implements CommandLineRunner {
 	@Autowired
 	private MqttReceiveConfig mqttReceiveConfig;
 
-
+	@Value("${spring.data.mongodb.uri}")
+	private String mongouri;
+	
+	@Value("${spring.datasource.url}")
+	private String mysqlurl;
+	
+	private static Logger logger = LogManager.getLogger(InitializingService.class);
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		logger.info("MongoDB数据库地址：");
+		logger.info(mongouri);
+		logger.info("mysql数据库地址：");
+		logger.info(mysqlurl);
 		mqttReceiveConfig.init();
 	}
 
