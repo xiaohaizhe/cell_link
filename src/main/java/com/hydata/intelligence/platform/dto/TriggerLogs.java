87487a02ec5ecf1db878ae8718229a019db3c2ba
jia32 <jiasimin@hiynn.com>
2019-03-19 11:04:43 +0800

    还原覆盖掉的部分：触发器日志，触发信息增量接口

--- src/main/java/com/hydata/intelligence/platform/dto/TriggerLogs.java
+++ src/main/java/com/hydata/intelligence/platform/dto/TriggerLogs.java
@@ -5,42 +5,56 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Date;
+
 @Entity
 public class TriggerLogs {
-	@Id
-	@GeneratedValue(generator = "IdGenerator")
+    @Id
+    @GeneratedValue(generator = "IdGenerator")
     @GenericGenerator(name = "IdGenerator",strategy = "com.hydata.intelligence.platform.utils.IdGenerator",
-                        parameters = {})
+            parameters = {})
     private long id;
 
     private long triggerId;
 
     private String msg;
 
-    
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
+    private Date send_time;
+
     public long getId() {
-		return id;
-	}
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public long getTriggerId() {
-		return triggerId;
-	}
+    public long getTriggerId() {
+        return triggerId;
+    }
 
-	public void setTriggerId(long triggerId) {
-		this.triggerId = triggerId;
-	}
+    public void setTriggerId(long triggerId) {
+        this.triggerId = triggerId;
+    }
 
-	public String getMsg() {
+    public String getMsg() {
         return msg;
     }
 
     public void setMsg(String msg) {
         this.msg = msg == null ? null : msg.trim();
+    }
+
+    public Date getSend_time() {
+        return send_time;
+    }
+
+    public void setSend_time(Date create_time) {
+        this.send_time = send_time;
     }
 
     @Override
@@ -52,6 +66,7 @@ public class TriggerLogs {
         sb.append(", id=").append(id);
         sb.append(", triggerId=").append(triggerId);
         sb.append(", msg=").append(msg);
+        sb.append(", send_time=").append(send_time);
         sb.append("]");
         return sb.toString();
     }
