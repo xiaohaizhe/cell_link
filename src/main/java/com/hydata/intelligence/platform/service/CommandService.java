88b3afd6000d296576ee03dcb35a89ff154b9818
jia32 <jiasimin@hiynn.com>
2019-03-26 17:16:35 +0800

    命令日志：添加下发失败的日志

--- src/main/java/com/hydata/intelligence/platform/service/CommandService.java
+++ src/main/java/com/hydata/intelligence/platform/service/CommandService.java
@@ -19,6 +19,7 @@ import com.hydata.intelligence.platform.utils.MongoDBUtils;
 import com.hydata.intelligence.platform.utils.MqttClientUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -128,7 +129,7 @@ public class CommandService {
         }
 
         //将16进制的content转换为字符串格式
-        if (type ==1){
+        if (type == 1) {
             content = content.replace(" ", "");
             byte[] baKeyword = new byte[content.length() / 2];
             for (int i = 0; i < baKeyword.length; i++) {
@@ -146,84 +147,93 @@ public class CommandService {
         }
 
         Optional<Device> deviceOptional = deviceRepository.findById(topic);
-        if(deviceOptional.isPresent()) {
+        if (deviceOptional.isPresent()) {
             Device device = deviceOptional.get();
-            //Optional<Product> productOptional = productRepository.findById(device.getProduct_id());
-            //if(productOptional.isPresent()) {
-            //boolean isMqtt = productOptional.get().getProtocolId()==1;
-            //        if (isMqtt) {
-                        try {
-                            // 创建命令消息
-                            MqttMessage message = new MqttMessage(content.getBytes());
-                            // 设置消息的服务质量
-                            message.setQos(mqtt.getQos());
-                            // 发布消息
-                            MqttClientUtil.getInstance().publish(String.valueOf(topic), message);
-                            //mqttHandler.publish(topic,content,true);
-                            /**
-                             * haizhe
-                             * (1) 存入指令log，
-                             * 此处不需要disconnect
-                             */
-                            logger.info("向设备" + topic + "发送了命令：" + content);
-                            //储存命令下发日志
-                            CmdLogs cmdLog = new CmdLogs();
-                            cmdLog.setId(System.currentTimeMillis());
-                            cmdLog.setDevice_id(topic);
-                            cmdLog.setMsg(content);
-                            cmdLog.setProductId(device.getProduct_id());
-                            Date date = new Date();
-                            cmdLog.setSendTime(date);
-                            cmdLog.setUserId(userid);
-                            cmdLog.setRes_code(0);
-                            cmdLog.setRes_msg("命令已发往设备");
-                            // 断开连接
-                            //MqttReceiveConfig.sendClient.disconnect();
-                            // 关闭客户端
-                            //sampleClient.close();
-                            //System.exit(0);
-                        } catch (Exception me) {
-                            //System.err.println("reason " + me.getReasonCode());
-                            //System.err.println("msg " + me.getMessage());
-                            //System.err.println("loc " + me.getLocalizedMessage());
-                            //System.err.println("cause " + me.getCause());
-                            //System.err.println("excep " + me);
-                            //me.printStackTrace();
-                            logger.error("向设备：" + topic + "下发命令失败");
-                            //logger.debug("reason " + me.getReasonCode());
-                            logger.debug("msg " + me.getMessage());
-                            logger.debug("loc " + me.getLocalizedMessage());
-                            logger.debug("cause " + me.getCause());
-                            logger.debug("excep " + me);
-                            me.printStackTrace();
-                            return RESCODE.PRODUCT_ID_NOT_EXIST.getJSONRES();
-                        }
-/*                    } else {
-                        logger.info("产品协议不支持命令下发");
-                        return RESCODE.NO_CHANGES.getJSONRES();
-                    }*/
+            Optional<Product> productOptional = productRepository.findById(device.getProduct_id());
+            if (productOptional.isPresent()) {
+                boolean isMqtt = productOptional.get().getProtocolId() == 1;
+                if (isMqtt) {
+                    try {
+                        // 创建命令消息
+                        MqttMessage message = new MqttMessage(content.getBytes());
+                        // 设置消息的服务质量
+                        message.setQos(mqtt.getQos());
+                        // 发布消息
+                        MqttClientUtil.getInstance().publish(String.valueOf(topic), message);
+                        //mqttHandler.publish(topic,content,true);
+                        /**
+                         * haizhe
+                         * (1) 存入指令log，
+                         * 此处不需要disconnect
+                         */
+                        logger.info("向设备" + topic + "发送了命令：" + content);
+                        //储存命令下发日志
+                        CmdLogs cmdLog = new CmdLogs();
+                        cmdLog.setId(System.currentTimeMillis());
+                        cmdLog.setDevice_id(topic);
+                        cmdLog.setMsg(content);
+                        cmdLog.setProductId(device.getProduct_id());
+                        Date date = new Date();
+                        cmdLog.setSendTime(date);
+                        cmdLog.setUserId(userid);
+                        cmdLog.setRes_code(0);
+                        cmdLog.setRes_msg("命令已发往设备");
+                        // 断开连接
+                        //MqttReceiveConfig.sendClient.disconnect();
+                        // 关闭客户端
+                        //sampleClient.close();
+                        //System.exit(0);
+                    } catch (MqttException me) {
+                        //System.err.println("reason " + me.getReasonCode());
+                        //System.err.println("msg " + me.getMessage());
+                        //System.err.println("loc " + me.getLocalizedMessage());
+                        //System.err.println("cause " + me.getCause());
+                        //System.err.println("excep " + me);
+                        //me.printStackTrace();
+                        logger.error("向设备：" + topic + "下发命令失败");
+                        //logger.debug("reason " + me.getReasonCode());
+                        logger.debug("msg " + me.getMessage());
+                        logger.debug("loc " + me.getLocalizedMessage());
+                        logger.debug("cause " + me.getCause());
+                        logger.debug("excep " + me);
+                        me.printStackTrace();
+                        CmdLogs cmdLog = new CmdLogs();
+                        cmdLog.setId(System.currentTimeMillis());
+                        cmdLog.setDevice_id(topic);
+                        cmdLog.setMsg(content);
+                        cmdLog.setProductId(device.getProduct_id());
+                        Date date = new Date();
+                        cmdLog.setSendTime(date);
+                        cmdLog.setUserId(userid);
+                        cmdLog.setRes_code(1);
+                        cmdLog.setRes_msg("命令发送失败");
+                        return RESCODE.FAILURE.getJSONRES(me);
+                    }
+                } else {
+                    logger.info("产品协议不支持命令下发");
+                    return RESCODE.NO_CHANGES.getJSONRES();
+                }
 /*             } else {
                  logger.error("产品id未找到,向设备："+topic+"下发命令失败");
                  return RESCODE.DEVICE_ID_NOT_EXIST.getJSONRES();
              }*/
-             logger.info("向设备"+topic+"成功发送了命令："+content);
-             return RESCODE.SUCCESS.getJSONRES();
-        }else {
-            logger.error("设备信息未找到"+topic+"，命令发送失败");
-    		return RESCODE.DEVICE_ID_NOT_EXIST.getJSONRES();
-    	}
-    	
-           
-       
+            } else {
+                logger.error("设备信息未找到" + topic + "，命令发送失败");
+                return RESCODE.DEVICE_ID_NOT_EXIST.getJSONRES();
+            }
 
-        //存储命令日志
 
-        //接受回执信息
+            //存储命令日志
+
+            //接受回执信息
 //      // 构建消息
 //      Message<String> messages = MessageBuilder.withPayload(content).setHeader(MqttHeaders.TOPIC, topic).build();
 //      // 发送消息
 //      mqttHandler.handleMessage(messages);
 
-        
+        }
+        logger.info("向设备" + topic + "成功发送了命令：" + content);
+        return RESCODE.SUCCESS.getJSONRES();
     }
+
 }
