b2affc3d101532512aad6bf9ee10d331d5e39cd3
pyt <2630451673@qq.com>
2019-06-27 14:48:54 +0800

    1.修改接口返回参数 2.字段id长度超过17位，必须转成String传给前端，否则id数据会错误

--- src/main/java/com/hydata/intelligence/platform/utils/WebConfig.java
+++ src/main/java/com/hydata/intelligence/platform/utils/WebConfig.java
@@ -1,10 +1,13 @@
 package com.hydata.intelligence.platform.utils;
 
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringBootConfiguration;
@@ -67,6 +70,11 @@ public class WebConfig extends WebMvcConfigurationSupport{
         fastJsonConfig.setSerializerFeatures(
                 SerializerFeature.PrettyFormat
         );
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+        fastJsonConfig.setSerializeConfig(serializeConfig);
         fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
         List<MediaType> supportedMediaTypes = new ArrayList<>();
         supportedMediaTypes.add(MediaType.APPLICATION_JSON);
