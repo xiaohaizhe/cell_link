202a5f17a89fb91b47eaa6f3eb601c6f0aa12cef
jia32 <jiasimin@hiynn.com>
2019-08-22 10:49:47 +0800

    降低while(true)占用CPU太高问题

--- src/main/java/com/hydata/intelligence/platform/utils/CommandHandlerThread.java
+++ src/main/java/com/hydata/intelligence/platform/utils/CommandHandlerThread.java
@@ -21,25 +21,29 @@ public class CommandHandlerThread extends Thread{
     public void run() {
         while(true)
         {
-            if(MqttClientUtil.getCommandQueue()!=null &&!MqttClientUtil.getCommandQueue().isEmpty()){
-                try {
-                    logger.info("尝试下发命令");
-                    CommandHandlerModel model = MqttClientUtil.getCommandQueue().poll(2, TimeUnit.SECONDS);
-                    // 创建命令消息
-                    MqttMessage message = new MqttMessage(model.getCmd().getBytes());
-                    // 设置消息的服务质量
-                    logger.info("准备发送命令， MQTT连接情况：" + MqttClientUtil.getInstance().isConnected());
-                    // 发布消息
-                    MqttClientUtil.getInstance().publish(model.getDeviceId() + "/cmd", model.getCmd().getBytes(),
-                            mqtt.getQos(), false);
-                    //mqttHandler.publish(topic,content,true);
-                    logger.info("向设备" + model.getDeviceId() + "发送了命令：" + model.getCmd());
-                }catch (Exception e) {
+            try {
+
+                sleep(1);
+            if(MqttClientUtil.getCommandQueue()!=null &&!MqttClientUtil.getCommandQueue().isEmpty()) {
+                logger.info("尝试下发命令");
+                CommandHandlerModel model = MqttClientUtil.getCommandQueue().poll(2, TimeUnit.SECONDS);
+                // 创建命令消息
+                MqttMessage message = new MqttMessage(model.getCmd().getBytes());
+                // 设置消息的服务质量
+                logger.info("准备发送命令， MQTT连接情况：" + MqttClientUtil.getInstance().isConnected());
+                // 发布消息
+                MqttClientUtil.getInstance().publish(model.getDeviceId() + "/cmd", model.getCmd().getBytes(),
+                        mqtt.getQos(), false);
+                //mqttHandler.publish(topic,content,true);
+                logger.info("向设备" + model.getDeviceId() + "发送了命令：" + model.getCmd());
+
+            }
+            }catch (Exception e) {
                     logger.error("MQTT命令下发失败");
                     e.printStackTrace();
                 }
             }
         }
-    }
+
 
 }
--- src/main/java/com/hydata/intelligence/platform/utils/EmailHandlerThread.java
+++ src/main/java/com/hydata/intelligence/platform/utils/EmailHandlerThread.java
@@ -22,49 +22,51 @@ public class EmailHandlerThread extends Thread{
 	public void run() {
 		while(true)
 		{
-			if(MqttClientUtil.getEmailQueue()!=null &&!MqttClientUtil.getEmailQueue().isEmpty()){
-				try {
-					logger.info("尝试发送邮件");
-					EmailHandlerModel model = MqttClientUtil.getEmailQueue().poll(2, TimeUnit.SECONDS);
-					/**
-					 * haizhe
-					 * 取出来后进行发邮件处理
-					 * TODO
-					 */
-					String host = "smtp.163.com"; // 指定发送邮件的主机smtp.qq.com(QQ)|smtp.163.com(网易)
-					Properties properties = System.getProperties();// 获取系统属性
-					properties.setProperty("mail.smtp.host", host);// 设置邮件服务器
-					properties.setProperty("mail.smtp.auth", "true");// 打开认证
-					String email = model.getEmail();
+			try {
+				sleep(1);
+				if (MqttClientUtil.getEmailQueue() != null && !MqttClientUtil.getEmailQueue().isEmpty()) {
 
-					// 1.获取默认session对象
-					Session session = Session.getDefaultInstance(properties, new Authenticator() {
-						public PasswordAuthentication getPasswordAuthentication() {
-							return new PasswordAuthentication(FROM, VCode); // 发件人邮箱账号、授权码
-						}
-					});
-					//session.setDebug(true);
-					// 2.创建邮件对象
-					Message message = new MimeMessage(session);
-					// 2.1设置发件人
-					message.setFrom(new InternetAddress(FROM));
-					// 2.2设置接收人
-					message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-					// 2.3设置邮件主题
-					message.setSubject("智能感知平台|触发器警报");
-					// 2.4设置邮件内容
-					String time = sdf.format(model.getCreateTime());
+				logger.info("尝试发送邮件");
+				EmailHandlerModel model = MqttClientUtil.getEmailQueue().poll(2, TimeUnit.SECONDS);
+				/**
+				 * haizhe
+				 * 取出来后进行发邮件处理
+				 * TODO
+				 */
+				String host = "smtp.163.com"; // 指定发送邮件的主机smtp.qq.com(QQ)|smtp.163.com(网易)
+				Properties properties = System.getProperties();// 获取系统属性
+				properties.setProperty("mail.smtp.host", host);// 设置邮件服务器
+				properties.setProperty("mail.smtp.auth", "true");// 打开认证
+				String email = model.getEmail();
 
-					String content = "<html><p><b>【Hydata海云智能感知平台】</b></p><p><b>触发器警报！</b></p><p>设备: "+
-                            model.getDeviceName()+"（设备id为"+model.getDeviceSn()+")"+"的数据流"+model.getDmName()+"于"+
-                            time+"时传来了数据"+model.getDataValue()+"，符合触发条件:"+model.getDataValue()+
-                            model.getTriggerSymbol()+model.getCriticalValue()+"触发了警报。</p></html>";
-					message.setContent(content, "text/html;charset=UTF-8");
-					logger.info("邮件内容："+content);
-					// 3.发送邮件
-					Transport.send(message);
+				// 1.获取默认session对象
+				Session session = Session.getDefaultInstance(properties, new Authenticator() {
+					public PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(FROM, VCode); // 发件人邮箱账号、授权码
+					}
+				});
+				//session.setDebug(true);
+				// 2.创建邮件对象
+				Message message = new MimeMessage(session);
+				// 2.1设置发件人
+				message.setFrom(new InternetAddress(FROM));
+				// 2.2设置接收人
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+				// 2.3设置邮件主题
+				message.setSubject("智能感知平台|触发器警报");
+				// 2.4设置邮件内容
+				String time = sdf.format(model.getCreateTime());
 
-				} catch (Exception e) {
+				String content = "<html><p><b>【Hydata海云智能感知平台】</b></p><p><b>触发器警报！</b></p><p>设备: " +
+						model.getDeviceName() + "（设备id为" + model.getDeviceSn() + ")" + "的数据流" + model.getDmName() + "于" +
+						time + "时传来了数据" + model.getDataValue() + "，符合触发条件:" + model.getDataValue() +
+						model.getTriggerSymbol() + model.getCriticalValue() + "触发了警报。</p></html>";
+				message.setContent(content, "text/html;charset=UTF-8");
+				logger.info("邮件内容：" + content);
+				// 3.发送邮件
+				Transport.send(message);
+				}
+				}catch (Exception e) {
 					logger.error("触发器邮件发送失败");
 					e.printStackTrace();
 				}
@@ -72,4 +74,3 @@ public class EmailHandlerThread extends Thread{
 		}
 	}
 
-}
